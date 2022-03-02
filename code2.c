13e790a6a9fa4d085da9d80fed7117c888766e24
Alex De george <ade-geor@F2N058.42adel.org.au>
2022-03-02 16:01:26 +1030

    actual code

--- code2.c
+++ code2.c
@@ -1,1 +1,35 @@
-this is also code
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ade-geor <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/15 11:15:39 by ade-geor          #+#    #+#             */
+/*   Updated: 2021/09/15 11:15:40 by ade-geor         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s1)
+{
+	int		i;
+	int		size;
+	char	*dup;
+
+	i = 0;
+	size = 0;
+	while (s1[size])
+		size++;
+	dup = malloc((size + 1) * sizeof(char));
+	if (!dup)
+		return (NULL);
+	while (i < size)
+	{
+		dup[i] = s1[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
--- code3.c
+++ code3.c
@@ -1,1 +1,41 @@
-more code in here
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ade-geor <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/15 11:14:10 by ade-geor          #+#    #+#             */
+/*   Updated: 2021/09/15 11:14:11 by ade-geor         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	size_t			i;
+	unsigned char	*dptr;
+	unsigned char	*sptr;
+
+	dptr = (unsigned char *)dst;
+	sptr = (unsigned char *)src;
+	i = 0;
+	if (dptr < sptr)
+	{
+		while (i < len)
+		{
+			dptr[i] = sptr[i];
+			i++;
+		}
+	}
+	else
+	{
+		while (i < len)
+		{
+			dptr[len - i - 1] = sptr[len - i - 1];
+			i++;
+		}
+	}
+	return (dst);
+}
